5633586b3c849975810a38946223e5046af6cb2b
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-05-24 22:35:03 +0000

    .

---
+++ postgres-experiments-22-05-remote-client/shielded-zfs/run_script.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python3 long_runs.py 20 3 && python3 long_runs.py 50 1 && python3 long_runs.py 50 2 && python3 long_runs.py 50 3 && python3 long_runs.py 60 1 && python3 long_runs.py 60 2 && python3 long_runs.py 60 3
