3ddfbd0a9bf64f89f32d1eff71f7fe5e949d4fe5
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-06-02 17:59:13 +0000

    updates on the script

--- scripts/run_postgres.sh
+++ scripts/run_postgres.sh
@@ -35,6 +35,7 @@ cleanup_docker_ext4() {
 }
 
 mount_docker_zfs() {
+    find_disks
     CUR_DIR=$(pwd)
     ZFS_DIR="/home/azureuser/original_zfs"
     cd ${ZFS_DIR}
@@ -45,6 +46,37 @@ mount_docker_zfs() {
     sudo systemctl start docker
 }
 
+mount_docker_shielded_zfs() {
+    find_disks
+    CUR_DIR=$(pwd)
+    ZFS_DIR="/home/azureuser/zfs"
+    cd ${ZFS_DIR}
+    sh autogen.sh && ./configure && make -s -j
+    sudo make install && sudo ldconfig && sudo depmod && sudo ./scripts/zfs.sh
+    cd ${CUR_DIR}
+    sudo zpool create -f zpool-docker -m /var/lib/docker ${DISKS}
+    sudo systemctl start docker
+}
+
+stop_docker_shielded_zfs() {
+    echo "Stopping docker ..."
+    sudo systemctl stop docker && sudo systemctl stop docker.socket
+    sudo systemctl stop docker && sudo systemctl stop docker.socket
+    cleanup_docker_shielded_zfs
+}
+
+unmount_shielded_zfs() {
+    CUR_DIR=$(pwd)
+    ZFS_DIR="/home/azureuser/zfs"
+    cd ${ZFS_DIR}
+
+    sudo ./scripts/zfs.sh -u
+    sudo make uninstall; sudo ldconfig; sudo depmod
+    make clean && export list="$(find -name .deps)" && for elem in $list; do sudo rm -rf $elem; done && sudo rm -rf __pycache__/ aclocal.m4 build/ config.log config.status  configure libtool stamp-h1 zfs_config.h.in zfs_config.h Makefile.in Makefile zfs.release configure~ zfs_config.h.in~
+    cd ${CUR_DIR}
+    sudo wipefs -a $DISKS
+
+}
 
 unmount_zfs() {
     CUR_DIR=$(pwd)
@@ -57,6 +89,14 @@ unmount_zfs() {
     cd ${CUR_DIR}
     sudo wipefs -a $DISKS
 
+}
+
+cleanup_docker_shielded_zfs() {
+    find_disks
+    echo "disks are: $DISKS"
+    sudo zpool destroy zpool-docker
+    sudo rm -rf /var/lib/docker
+    unmount_shielded_zfs
 }
 
 cleanup_docker_zfs() {
@@ -84,6 +124,7 @@ stop_docker_zfs() {
 }
 
 mount_docker_ext4() {
+    find_disks
     sudo mdadm --create --verbose /dev/md0 --level=0 --raid-devices=$(echo "$DISKS" | awk '{print NF}') $DISKS
     sudo mkdir -p /var/lib/docker
     sudo mkfs.ext4 -F /dev/md0
@@ -104,24 +145,40 @@ start_docker_ext4() {
 run_postgres_ext4() {
     echo "Running PostgreSQL container ..."
     serialized_date=$(date +%Y%m%d_%H%M)
-
-    mkdir -p $(pwd)/ext4_${serialized_date}
+    CUR_DIR=$(pwd)/ext4_${serialized_date}
+    mkdir -p ${CUR_DIR}
+    cd ${CUR_DIR}
 
     BASE_DIR_EXT4="/home/azureuser/zfs-docs/postgres-experiments-22-05-remote-client/ext4"
     cp ${BASE_DIR_EXT4}/long_runs.py .
-    #python3 long_runs.py 10 1
- 
+    python3 long_runs.py 10 11
+    cd ../
 }
 
 run_postgres_zfs() {
     echo "Running PostgreSQL container ..."
     serialized_date=$(date +%Y%m%d_%H%M)
-
-    mkdir -p $(pwd)/zfs_${serialized_date}
-
+    CUR_DIR=$(pwd)/zfs_${serialized_date}
+    mkdir -p ${CUR_DIR}
+    cd ${CUR_DIR}
+    
     BASE_DIR_EXT4="/home/azureuser/zfs-docs/postgres-experiments-22-05-remote-client/zfs"
     cp ${BASE_DIR_EXT4}/long_runs.py .
-    #python3 long_runs.py 10 1
+    python3 long_runs.py 10 11
+    cd ../
+ 
+}
+
+run_postgres_shielded_zfs() {
+    echo "Running PostgreSQL container ..."
+    serialized_date=$(date +%Y%m%d_%H%M)
+    CUR_DIR=$(pwd)/shielded_zfs_${serialized_date}
+    mkdir -p ${CUR_DIR}
+    cd ${CUR_DIR}
+    BASE_DIR_EXT4="/home/azureuser/zfs-docs/postgres-experiments-22-05-remote-client/zfs"
+    cp ${BASE_DIR_EXT4}/long_runs.py .
+    python3 long_runs.py 10 11
+    cd ../
  
 }
 
@@ -136,4 +193,10 @@ stop_docker_ext4
 
 mount_docker_zfs
 run_postgres_zfs
-unmount_zfs
+stop_docker_zfs
+
+
+
+mount_docker_shielded_zfs
+run_postgres_shielded_zfs
+stop_docker_shielded_zfs
