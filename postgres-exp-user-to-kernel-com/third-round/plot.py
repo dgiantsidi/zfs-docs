d40d3f2eae3066b29c2308e05331deb22654be67
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-07-08 14:29:31 +0000

    updates

--- postgres-exp-user-to-kernel-com/third-round/plot.py
+++ postgres-exp-user-to-kernel-com/third-round/plot.py
@@ -23,8 +23,8 @@ throughput_szfs = [606.17, 993.87, 1557.39, 1741.99]
 # Plot
 plt.figure(figsize=(8, 6))
 
-plt.plot(throughput_zfs, latency_zfs, 'o-', label='ZFS', color='blue')
-plt.plot(throughput_szfs, latency_szfs, 's--', label='Shielded ZFS', color='green')
+plt.plot(throughput_zfs, latency_zfs, 'o-', label='Shielded ZFS', color='blue')
+plt.plot(throughput_szfs, latency_szfs, 's--', label='Shielded ZFS w/ acked cmts from userspace', color='green')
 
 # Add text on top of the data points
 for i in range(len(latency_szfs)):
